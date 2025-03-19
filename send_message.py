fa2204a995ff9c0eeee529895fc7402bfe9acbf3
Kiki979 <kiki979@github.com>
2025-03-19 16:37:41 +0100

    add script for send messages and github actions

---
+++ send_message.py
@@ -0,0 +1,41 @@
+import http.client
+import urllib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def send_push_notification(message, title="Testung", sound="magic"):
+   
+    token = os.getenv('TOKEN')
+    user = os.getenv('USER')
+
+    conn = http.client.HTTPSConnection("api.pushover.net", 443)
+    data = urllib.parse.urlencode({
+        "token": token,
+        "user": user,
+        "title": title,
+        "message": message,
+        "priority": 1,
+        "sound": sound
+    })
+    headers = {"Content-type": "application/x-www-form-urlencoded"}
+
+    try:
+        conn.request("POST", "/1/messages.json", data, headers)
+        response = conn.getresponse()
+
+        if response.status == 200:
+            print("✅ Push-Nachricht erfolgreich gesendet!")
+        else:
+            print(f"❌ Fehler beim Senden der Push-Nachricht: {response.status}")
+            print("Antwort:", response.read().decode())
+
+    except Exception as e:
+        print(f"⚠️ Ausnahmefehler beim Senden der Push-Nachricht: {e}")
+
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    send_push_notification("Login erfolgreich!")
